e8fe3690e17deca5e9651843532da876108fcbfa
Abhishek Kumar Jha <abhishekj@cal.fi-tek.co.in>
2019-05-02 20:22:30 +0530

    Initial Commit

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+    $("#theForm").hide();
+
+    $("#buyButton").on("click", function () {
+        console.log("Buying Item.");
+    });
+
+    $(".product-props li").on("click", function () {
+        console.log("Clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.toggle(100);
+    });
+
+});
+
+
+
+
